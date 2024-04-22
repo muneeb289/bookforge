03efad19c56ffa70944c380c22c72e508ac36f74
muneeb289 <muneeb289@yahoo.com>
2024-04-22 10:53:55 +0500

    added BookTable

--- front/src/pages/Layout/Layout.js
+++ front/src/pages/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
